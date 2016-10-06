93c34761257ae35a4aa96b7e7b0e0e5303b1fa7a
Luis A Penafort <lpenafort88@hotmail.com>
2016-10-05 22:42:52 -0400

    Removed All static Html page references Setup 404 Error page and redirect Commented out seccion of the main page not going to be used at the time

---
+++ IDDPMIRas/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDDPMIRas.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;       
+            return View("~/Views/Shared/404.cshtml");
+        }
+    }
+}
